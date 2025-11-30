f70de0bba985e754b554baa21e69d480577045a1
aikimind <aikimind@mail.ru>
2025-11-30 12:04:37 +0300

    users

--- js/components/users.js
+++ js/components/users.js
@@ -20,17 +20,25 @@ class Users extends React.Component {
     ]
 
     render() {
-        return (
-           <div>
-            {
-                this.users.map((el)=>(
-                    <div className="user" key={el.id}>
-                        <h3>{el.firstname} {el.lastname}</h3>
-                        <p>{el.bio}</p>
-                    </div>
-                ))
-            }
-           </div>        
+        if (this.users.length > 0)
+            return (
+            <div>
+                {
+                    this.users.map((el)=>(
+                        <div className="user" key={el.id}>
+                            <h3>{el.firstname} {el.lastname}</h3>
+                            <p>{el.bio}</p>
+                            <b>{el.isHappy ? 'Счастлив :)' : 'Не особо :('}</b>
+                        </div>
+                    ))
+                }
+            </div>        
+            )
+        else
+            return(
+                <div className="user">
+                    <h3>Пользователей нет</h3>
+                </div>
         )
     }
 }
