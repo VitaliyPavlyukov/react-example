b95d8875c102f00b0700e11a852a37960177d96f
Виталий Павлюков <aikimind@mail.ru>
2025-12-06 11:40:15 +0300

    edit user

--- js/components/AddUser.js
+++ js/components/AddUser.js
@@ -10,17 +10,28 @@ class AddUser extends React.Component {
             age: 1,
             isHappy: false
         }
+
+        // Обновление значений ввода        
+        if (this.props.user){
+            this.state.firstname = this.props.user.firstname;   
+            this.state.lastname = this.props.user.lastname;  
+            this.state.bio = this.props.user.bio;  
+            this.state.age = this.props.user.age;  
+            this.state.isHappy = this.props.user.isHappy;                       
+        }
     }
 
-    render() {
+    render() {        
+
         return (
             <form ref={(el)=> this.myForm = el}>
-                <input placeholder="Имя" onChange={(e) => this.setState({firstname: e.target.value})}></input>
-                <input placeholder="Фамилия" onChange={(e) => this.setState({lastname: e.target.value})}></input>
-                <textarea placeholder="Биография" onChange={(e) => this.setState({bio: e.target.value})}></textarea>
-                <input placeholder="Возраст" onChange={(e) => this.setState({age: e.target.value})}></input>
+                <input placeholder="Имя" value={this.state.firstname} onChange={(e) => this.setState({firstname: e.target.value})}></input>
+                <h2>Имя: </h2>
+                <input placeholder="Фамилия" value={this.state.lastname} onChange={(e) => this.setState({lastname: e.target.value})}></input>
+                <textarea placeholder="Биография" value={this.state.bio} onChange={(e) => this.setState({bio: e.target.value})}></textarea>
+                <input placeholder="Возраст" value={this.state.age} onChange={(e) => this.setState({age: e.target.value})}></input>
                 <label htmlFor="isHappy">Счастлив?</label>
-                <input type="checkbox" id="isHappy" onChange={(e) => this.setState({isHappy: e.target.checked})}></input>
+                <input type="checkbox" id="isHappy" checked={this.state.isHappy} onChange={(e) => this.setState({isHappy: e.target.checked})}></input>
                 <button type="button" onClick={() => {
                     this.myForm.reset()
                     this.userAdd = {
@@ -32,9 +43,9 @@ class AddUser extends React.Component {
                     }
                     if (this.props.user)
                         this.userAdd.id = this.props.user.id
-                    this.props.onAdd(this.userAdd)
+                    this.props.onAdd(this.userAdd)                    
                 }
-                }>Добавить</button>
+                }>{(this.props.user) ? "Сохранить" : "Добавить"}</button>
             </form>        
         )
     }
